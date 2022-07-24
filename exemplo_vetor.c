64f32f5152da9e54d30173095cadc2ce21180975
zapsys <zapsyssolutions@gmail.com>
2022-07-24 20:59:40 -0300

    Exemplo vetor com 10 elementos

---
+++ exemplo_vetor.c
@@ -0,0 +1,11 @@
+/* Vetor 'dados' com 10 elementos */
+#include <stdio.h>
+int main() {
+    int i, dados[10];
+    for (i = 0; i < 10; i++) {
+        dados[i] = i;
+        printf("%d",dados[i]);
+    }
+    return 0;
+}
+
